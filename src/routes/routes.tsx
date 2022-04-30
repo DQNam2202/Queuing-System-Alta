19338806b0d2a80fd14f0ad0cb87f54c4f81da20
Nam Duong <duongquocnam224400@gmail.com>
2022-04-30 08:46:43 +0700

    done-gear_level_devide-not-responsive

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,6 +10,7 @@ import Page404 from '../components/Page404';
 import Login from '../pages/Home/Login';
 import CreateNewPassword from '../pages/Home/CreateNewPassword';
 import ResetPassword from '../pages/Home/ResetPassword';
+import GearLevelDevice from '../pages/Users/GearLevelDevice';
 
 export const publicRoutes: RouteObject[] = [
   {
@@ -41,6 +42,14 @@ export const publicRoutes: RouteObject[] = [
     element: (
       <PublicRoute>
         <CreateNewPassword />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: '/leveldevice',
+    element: (
+      <PublicRoute>
+        <GearLevelDevice />
       </PublicRoute>
     ),
   },
