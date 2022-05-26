0d53f5576f6bc94806bc7b446d342c4856af7fb5
Nam Duong <duongquocnam224400@gmail.com>
2022-05-26 08:30:17 +0700

    Completed under graves templated

--- src/routes/privateRoute.tsx
+++ src/routes/privateRoute.tsx
@@ -1,14 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import AdminTemplate from "../templates/AdminTemplate";
+import AdminTemplate from '../templates/AdminTemplate';
 
-interface Props{
-    children : JSX.Element | JSX.Element[],
+interface Props {
+  children: JSX.Element | JSX.Element[];
 }
 
-const PrivateRoute = ({children} : Props) : JSX.Element=>{
-      return (
-        <AdminTemplate>{children}</AdminTemplate>
-      )
-  };
-  
-  export default PrivateRoute
+const PrivateRoute = ({ children }: Props): JSX.Element => {
+  return <AdminTemplate>{children}</AdminTemplate>;
+};
+
+export default PrivateRoute;
